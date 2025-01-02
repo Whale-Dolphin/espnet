cbf2a1cdfab598c3c4546d7780e5dca554c0a820
Whale-Dolphin <whaledolphin666@gmail.com>
2025-01-02 04:33:14 +0000

    [feature]optimize conf and merge versa to tts

--- egs2/TEMPLATE/tts1/tts.sh
+++ egs2/TEMPLATE/tts1/tts.sh
@@ -118,15 +118,12 @@ speech_fold_length=800 # fold_length for speech data.
 
 # VERSA eval related
 skip_scoring=true # Skip scoring stages.
+skip_wer=true # Skip WER evaluation.
 whisper_tag=medium # Whisper model tag.
 whisper_dir=local/whisper # Whisper model directory.
 cleaner=whisper_en # Text cleaner for whisper model.
 hyp_cleaner=whisper_en # Text cleaner for hypothesis.
-
-versa_eval_params=(
-    mos
-    spk
-) # Parameters for VERSA evaluation.
+versa_config=versa.yaml # VERSA evaluation configuration.
 
 
 # Upload model related
@@ -1130,178 +1127,74 @@ fi
 
 if ! "${skip_scoring}"; then
     if [ ${stage} -le 9 ] && [ ${stop_stage} -ge 9 ]; then
-        ./scripts/utils/evaluate_asr.sh \
-            --whisper_tag ${whisper_tag} \
-            --whisper_dir ${whisper_dir} \
-            --cleaner ${cleaner} \
-            --hyp_cleaner ${hyp_cleaner} \
-            --inference_nj ${inference_nj} \
-            --nj ${nj} \
-            --gt_text ${data}/text \
-            --gpu_inference ${gpu_inference} \
-            ${gen_dir}/wav.scp ${gen_dir}/scoring/eval_wer
+        _gen_dir=${tts_exp}/${inference_tag}/${test_set}
+        _data=${data_feats}/${test_set}
+        if ! ${skip_wer}; then
+            ./scripts/utils/evaluate_asr.sh \
+                --whisper_tag ${whisper_tag} \
+                --whisper_dir ${whisper_dir} \
+                --cleaner ${cleaner} \
+                --hyp_cleaner ${hyp_cleaner} \
+                --inference_nj ${inference_nj} \
+                --nj ${nj} \
+                --gt_text ${_data}/text \
+                --gpu_inference ${gpu_inference} \
+                ${_gen_dir}/wav/wav_test.scp ${_gen_dir}/scoring/eval_wer
+            
+            echo "Finished WER evaluation, results are in ${_gen_dir}/scoring/eval_wer"
+        fi
 
-        for eval_item in versa_eval_params; do
-            eval_flag=eval_${eval_item}
-            if ${!eval_flag}; then
-                opts=
-                eval_dir=${tts_exp}/scoring/eval_${eval_item}; mkdir -p ${eval_dir}
+        echo "Scoring TTS evaluation via VERSA, using default ${versa_config}. You can visit https://github.com/shinjiwlab/versa?tab=readme-ov-file#list-of-metrics for more supported metrics."
+        _opts=
+        _eval_dir=${_gen_dir}/scoring/versa_eval
+        mkdir -p ${_eval_dir}
 
-                # (2) define pred, ref and config
-                if [ ${eval_item} == "mos" ]; then
-                    pred_file=${tts_exp}/wav.scp
-                    score_config=${mos_config}
-                    gt_file=
-                elif [ ${eval_item} == "spk" ]; then
-                    pred_file=${tts_exp}/wav.scp
-                    score_config=${spk_config}
-                    gt_file=${ref_dir}/utt2spk
-                fi
+        _pred_file=${_gen_dir}/wav/wav_test.scp
+        _score_config=${versa_config}
+        _gt_file=${_data}/wav_test.scp
 
-                # (3) split
-                _nj=$(min "${inference_nj}" "$(<${pred_file} wc -l)" )
+        _nj=$(( ${inference_nj} < $(wc -l < ${_pred_file}) ? ${inference_nj} : $(wc -l < ${_pred_file}) ))
 
-                split_files=""
-                for n in `seq ${_nj}`; do
-                    split_files+="${eval_dir}/pred.${n} "
-                done
-                utils/split_scp.pl ${pred_file} ${split_files}
-
-                if [ -n "${gt_file}" ]; then
-                    split_files=""
-                    for n in `seq ${_nj}`; do
-                        split_files+="${eval_dir}/gt.${n} "
-                    done
-                    utils/split_scp.pl ${gt_file} ${split_files}
-                    opts+="--gt ${eval_dir}/gt.JOB"
-                fi
-
-                # (4) score
-                if ${gpu_inference}; then
-                    _cmd="${cuda_cmd}"
-                    _ngpu=1
-                else
-                    _cmd="${decode_cmd}"
-                    _ngpu=0
-                fi
-
-                ${_cmd} --gpu "${_ngpu}" JOB=1:"${_nj}" "${eval_dir}"/eval_${eval_item}.JOB.log \
-                    ${python} -m versa.bin.espnet_scorer \
-                        --pred ${eval_dir}/pred.JOB \
-                        --score_config ${score_config} \
-                        --use_gpu ${gpu_inference} \
-                        --output_file ${eval_dir}/result.JOB.txt \
-                        --io soundfile \
-                        ${opts} || exit 1;
-
-                # (5) aggregate
-                pyscripts/utils/aggregate_eval.py \
-                    --logdir ${eval_dir} \
-                    --scoredir ${eval_dir} \
-                    --nj ${_nj}
-            fi
+        _split_files=""
+        for n in $(seq ${_nj}); do
+            _split_files+="${_eval_dir}/pred.${n} "
         done
+        utils/split_scp.pl ${_pred_file} ${_split_files}
+
+        if [ -n "${_gt_file}" ]; then
+            _split_files=""
+            for n in $(seq ${_nj}); do
+                _split_files+="${_eval_dir}/gt.${n} "
+            done
+            utils/split_scp.pl ${_gt_file} ${_split_files}
+            _opts+="--gt ${_eval_dir}/gt.JOB"
+        fi
+
+        if ${gpu_inference}; then
+            _cmd="${cuda_cmd}"
+            _ngpu=1
+        else
+            _cmd="${decode_cmd}"
+            _ngpu=0
+        fi
+
+        ${_cmd} --gpu "${_ngpu}" JOB=1:"${_nj}" "${_eval_dir}"/versa_eval.JOB.log \
+        python -W ignore -m versa.bin.scorer \
+            --pred ${_eval_dir}/pred.JOB \
+            --score_config ${_score_config} \
+            --cache_folder ${_eval_dir}/cache \
+            --use_gpu ${gpu_inference} \
+            --output_file ${_eval_dir}/result.JOB.txt \
+            --io soundfile
+
+        python pyscripts/utils/aggregate_tts_eval.py \
+            --logdir ${_eval_dir} \
+            --scoredir ${_eval_dir} \
+            --nj ${_nj}
+
+        echo "Finished scoring evaluation, results are in ${_eval_dir}"
     fi
 fi
-
-
-# if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
-#     # User VERSA
-#     for eval_item in mos spk; do
-#         eval_flag=eval_${eval_item}
-#         if ${!eval_flag}; then
-#             # (1) init
-#             opts=
-#             eval_dir=${gen_dir}/scoring/eval_${eval_item}; mkdir -p ${eval_dir}
-
-#             # (2) define pred, ref and config
-#             if [ ${eval_item} == "mos" ]; then
-#                 pred_file=${gen_dir}/wav.scp
-#                 score_config=${mos_config}
-#                 gt_file=
-#             elif [ ${eval_item} == "spk" ]; then
-#                 pred_file=${gen_dir}/wav.scp
-#                 score_config=${spk_config}
-#                 gt_file=${ref_dir}/utt2spk
-#             fi
-
-#             # (3) split
-#             _nj=$(min "${inference_nj}" "$(<${pred_file} wc -l)" )
-
-#             split_files=""
-#             for n in `seq ${_nj}`; do
-#                 split_files+="${eval_dir}/pred.${n} "
-#             done
-#             utils/split_scp.pl ${pred_file} ${split_files}
-
-#             if [ -n "${gt_file}" ]; then
-#                 split_files=""
-#                 for n in `seq ${_nj}`; do
-#                     split_files+="${eval_dir}/gt.${n} "
-#                 done
-#                 utils/split_scp.pl ${gt_file} ${split_files}
-#                 opts+="--gt ${eval_dir}/gt.JOB"
-#             fi
-
-#             # (4) score
-#             if ${gpu_inference}; then
-#                 _cmd="${cuda_cmd}"
-#                 _ngpu=1
-#             else
-#                 _cmd="${decode_cmd}"
-#                 _ngpu=0
-#             fi
-
-#             ${_cmd} --gpu "${_ngpu}" JOB=1:"${_nj}" "${eval_dir}"/eval_${eval_item}.JOB.log \
-#                 ${python} -m versa.bin.espnet_scorer \
-#                     --pred ${eval_dir}/pred.JOB \
-#                     --score_config ${score_config} \
-#                     --use_gpu ${gpu_inference} \
-#                     --output_file ${eval_dir}/result.JOB.txt \
-#                     --io soundfile \
-#                     ${opts} || exit 1;
-
-#             # (5) aggregate
-#             pyscripts/utils/aggregate_eval.py \
-#                 --logdir ${eval_dir} \
-#                 --scoredir ${eval_dir} \
-#                 --nj ${_nj}
-
-#         else
-#             log "Skip evaluting ${eval_item}"
-#         fi
-#     done
-# fi
-
-# if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
-#     log "Summarize the results"
-
-#     all_eval_results=
-#     metrics=
-
-#     if ${eval_wer}; then
-#         all_eval_results+="${gen_dir}/scoring/eval_wer/utt_result.txt "
-#         metrics+="wer "
-#     fi
-
-#     if ${eval_spk}; then
-#         all_eval_results+="${gen_dir}/scoring/eval_spk/utt_result.txt "
-#         metrics+="spk_similarity "
-#     fi
-
-#     if ${eval_mos}; then
-#         all_eval_results+="${gen_dir}/scoring/eval_mos/utt_result.txt "
-#         metrics+="utmos "
-#     fi
-
-#     ${python} pyscripts/utils/result_summary.py \
-#         --all_eval_results ${all_eval_results} \
-#         --key_file ${key_file} \
-#         --output_dir ${gen_dir}/scoring \
-#         --metrics ${metrics} \
-#         --nbest ${nbest} \
-#         > ${gen_dir}/scoring/final_result.txt
-# fi
 
 packed_model="${tts_exp}/${tts_exp##*/}_${inference_model%.*}.zip"
 if ! "${skip_packing}" && [ -z "${download_model}" ]; then
--- egs2/TEMPLATE/tts1/versa.sh
+++ egs2/TEMPLATE/tts1/versa.sh
@@ -1,6 +1,6 @@
 srctexts="data/${train_set}/text"
 test_set="eval1"
-data_feats="${dumpdir}/raw"
+data_feats="dump/raw"
 _data=${data_feats}/${test_set}
 tts_exp=exp/tts_train_full_band_multi_spk_vits_raw_phn_tacotron_g2p_en_no_space
 
@@ -19,8 +19,7 @@ whisper_dir=local/whisper # Whisper model directory.
 cleaner=whisper_en # Text cleaner for whisper model.
 hyp_cleaner=whisper_en # Text cleaner for hypothesis.
 
-mos_config=mos.yaml # MOS evaluation configuration.
-spk_config=spk.yaml # Speaker evaluation configuration.
+versa_config=versa.yaml # VERSA evaluation configuration.
 
 versa_eval_params=(
     # mos
@@ -43,61 +42,50 @@ if ! ${skip_wer}; then
         ${_gen_dir}/wav/wav_test.scp ${_gen_dir}/scoring/eval_wer
 fi
 
-for _eval_item in "${versa_eval_params[@]}"; do
-    _eval_flag=eval_${_eval_item}
-    if ${!_eval_flag}; then
-        _opts=
-        _eval_dir=${_gen_dir}/scoring/eval_${_eval_item}
-        mkdir -p ${_eval_dir}
+_opts=
+_eval_dir=${_gen_dir}/scoring/versa_eval
+mkdir -p ${_eval_dir}
 
-        if [ ${_eval_item} == "mos" ]; then
-            _pred_file=${_gen_dir}/wav/wav_test.scp
-            _score_config=${mos_config}
-            _gt_file=
-        elif [ ${_eval_item} == "spk" ]; then
-            _pred_file=${_gen_dir}/wav/wav_test.scp
-            _score_config=${spk_config}
-            _gt_file=${ref_dir}/utt2spk
-        fi
+_pred_file=${_gen_dir}/wav/wav_test.scp
+_score_config=${versa_config}
+_gt_file=${_data}/wav_test.scp
 
-        _nj=$(( ${inference_nj} < $(wc -l < ${_pred_file}) ? ${inference_nj} : $(wc -l < ${_pred_file}) ))
+_nj=$(( ${inference_nj} < $(wc -l < ${_pred_file}) ? ${inference_nj} : $(wc -l < ${_pred_file}) ))
 
-        _split_files=""
-        for n in $(seq ${_nj}); do
-            _split_files+="${_eval_dir}/pred.${n} "
-        done
-        utils/split_scp.pl ${_pred_file} ${_split_files}
-
-        if [ -n "${_gt_file}" ]; then
-            _split_files=""
-            for n in $(seq ${_nj}); do
-                _split_files+="${_eval_dir}/gt.${n} "
-            done
-            utils/split_scp.pl ${_gt_file} ${_split_files}
-            _opts+="--gt ${_eval_dir}/gt.JOB"
-        fi
-
-        if ${gpu_inference}; then
-            _cmd="${cuda_cmd}"
-            _ngpu=1
-        else
-            _cmd="${decode_cmd}"
-            _ngpu=0
-        fi
-
-        ${_cmd} --gpu "${_ngpu}" JOB=1:"${_nj}" "${_eval_dir}"/eval_${_eval_item}.JOB.log \
-        python -m versa.bin.scorer \
-            --pred ${_eval_dir}/pred.JOB \
-            --score_config ${_score_config} \
-            --cache_folder ${_eval_dir}/cache \
-            --use_gpu ${gpu_inference} \
-            --output_file ${_eval_dir}/result.JOB.txt \
-            --io soundfile \
-            ${_opts} || exit 1;
-
-        pyscripts/utils/aggregate_eval.py \
-            --logdir ${_eval_dir} \
-            --scoredir ${_eval_dir} \
-            --nj ${_nj}
-    fi
+_split_files=""
+for n in $(seq ${_nj}); do
+    _split_files+="${_eval_dir}/pred.${n} "
 done
+utils/split_scp.pl ${_pred_file} ${_split_files}
+
+if [ -n "${_gt_file}" ]; then
+    _split_files=""
+    for n in $(seq ${_nj}); do
+        _split_files+="${_eval_dir}/gt.${n} "
+    done
+    utils/split_scp.pl ${_gt_file} ${_split_files}
+    _opts+="--gt ${_eval_dir}/gt.JOB"
+fi
+
+if ${gpu_inference}; then
+    _cmd="${cuda_cmd}"
+    _ngpu=1
+else
+    _cmd="${decode_cmd}"
+    _ngpu=0
+fi
+
+${_cmd} --gpu "${_ngpu}" JOB=1:"${_nj}" "${_eval_dir}"/versa_eval.JOB.log \
+python -m versa.bin.scorer \
+    --pred ${_eval_dir}/pred.JOB \
+    --score_config ${_score_config} \
+    --cache_folder ${_eval_dir}/cache \
+    --use_gpu ${gpu_inference} \
+    --output_file ${_eval_dir}/result.JOB.txt \
+    --io soundfile \
+    ${_opts} 2>&1 | grep -i "info" || exit 1;
+
+python pyscripts/utils/aggregate_tts_eval.py \
+    --logdir ${_eval_dir} \
+    --scoredir ${_eval_dir} \
+    --nj ${_nj}
